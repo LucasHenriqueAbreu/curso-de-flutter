1bca741541b5cbfcb6c41d6546a25f7be827996f
Lucas Henrique de Abreu <lha@intranet.pitang.com>
2024-02-23 17:36:25 -0300

    exemplo de chat

---
+++ modulo1/streams/aula/chat/mainOne.dart
@@ -0,0 +1,40 @@
+// enviar_mensagem.dart
+import 'dart:math';
+import 'dart:async';
+
+StreamController<String> controller = StreamController<String>();
+
+Stream<String> getMessages() {
+  return controller.stream;
+}
+
+void sendMessage(String message) {
+  controller.add(message);
+}
+
+void close() {
+  controller.close();
+}
+
+void main() async {
+  getMessages().listen((message) {
+    print('Nova mensagem recebida: $message');
+  });
+
+  final List<String> mensagens = [
+    'Olá!',
+    'Como você está?',
+    'Qual é o seu nome?',
+    'Estou bem, obrigado!',
+    'Tenha um bom dia!',
+    'Tchau!'
+  ];
+
+  final random = Random();
+
+  while (true) {
+    final mensagem = mensagens[random.nextInt(mensagens.length)];
+    sendMessage(mensagem);
+    await Future.delayed(Duration(seconds: 2));
+  }
+}
